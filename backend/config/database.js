85d2f9f07a3c65698d734a495688d5b9d2854a5a
NikhilT27 <nikhilthakare14@gmail.com>
2020-09-15 23:54:00 +0530

    fixed strapi error

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         uri:
-          "mongodb://nikhilT27:Iamportfolio27@portfolio-strapi-backen-shard-00-00.o9n0o.mongodb.net:27017,portfolio-strapi-backen-shard-00-01.o9n0o.mongodb.net:27017,portfolio-strapi-backen-shard-00-02.o9n0o.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-j0dxbr-shard-0&authSource=admin&retryWrites=true&w=majority",
+          ,
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
