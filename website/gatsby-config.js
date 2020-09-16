6f148740177cdec38ffee5d9959616e5d526b9ca
NikhilT27 <nikhilthakare14@gmail.com>
2020-09-16 16:34:42 +0530

    add: google analytics

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -27,6 +27,12 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
